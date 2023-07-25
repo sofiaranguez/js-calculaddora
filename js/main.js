b713b17d8ba13f08cc4d427554af8332325ae8b9
sofia aranguez <sofiaranguez993@gmail.com>
2023-07-25 20:39:38 -0300

    segunda entrega con ayuda de tutoriales

--- js/main.js
+++ js/main.js
@@ -1,65 +1,113 @@
-//-------- proyecto de calculadora ------
-function calcular() {
-    const operacion = prompt("Elige una operación (suma, resta, multiplicacion, division):");
-    const num1 = parseInt(prompt("Ingresa el primer número:"));
-    const num2 = parseInt(prompt("Ingresa el segundo número:"));
+//proyecto de bingo , 2da pre entrega//
 
-    let resultado;
-    //-------- Bucle----------
-    while (isNaN(num1) || isNaN(num2)) {
-        alert("Por favor, ingresa números válidos.");
-        num1 = parseInt(prompt("Ingresa el primer número:"));
-        num2 = parseInt(prompt("Ingresa el segundo número:"));
-    }
-    // --------aplicamos switch------
-    switch (operacion) {
-        case "suma":
-            resultado = sumar(num1, num2);
-            break;
-        case "resta":
-            resultado = restar(num1, num2);
-            break;
-        case "multiplicacion":
-            resultado = multiplicar(num1, num2);
-            break;
-        case "division":
-            resultado = dividir(num1, num2);
-            break;
-        default:
-            resultado = "Operación inválida";
-    }
+let numinicial = 1, numfinal = 75
+let sigue, letra
+do {
+    numero = Math.round(Math.random() * (numinicial - numfinal) + numfinal);
+    if (numero >= 1 && numero <= 15) letra = "B"
+    if (numero >= 16 && numero <= 30) letra = "I"
+    if (numero >= 31 && numero <= 45) letra = "N"
+    if (numero >= 46 && numero <= 60) letra = "G"
+    if (numero >= 61 && numero <= 75) letra = "O"
 
-    alert("El resultado es: " + resultado);
-}
-//-------- funciones--------
-function sumar(a, b) {
-    return a + b;
-}
+    console.log(letra + numero);
 
-function restar(a, b) {
-    return a - b;
+    sigue = prompt("Desea sacar una bolilla? S/N")
+} while (sigue == "S" || sigue == "s")
+
+
+
+// registro de asistencias // no terminado
+
+
+// Definir una función de orden superior para crear el registro de asistencias
+function crearRegistro() {
+    let asistencias = {};
+
+    return function (alumno) {
+        if (!asistencias[alumno]) {
+            asistencias[alumno] = 1;
+        } else {
+            asistencias[alumno]++;
+        }
+
+        console.log(`Asistencia registrada para ${alumno}`);
+        console.log(`Número total de asistencias para ${alumno}: ${asistencias[alumno]}`);
+    };
 }
 
-function multiplicar(a, b) {
-    return a * b;
-}
+// Crear el registro de asistencias
+const tomarAsistencia = crearRegistro();
 
-function dividir(a, b) {
-    if (b === 0) {
-        return "Error: No se puede dividir por cero";
-    }
-    return a / b;
-}
+// Tomar asistencias para los alumnos usando prompt
+while (true) {
+    const alumno = prompt('Ingresa el nombre del alumno (o presiona Cancelar para terminar):');
 
-calcular();
-console.log(calcular)
+    if (alumno === null) {
+        break; // Salir del bucle si se presiona Cancelar
+        // Array de productos
+        const productos = [
+            { id: 1, nombre: 'Camiseta', precio: 20 },
+            { id: 2, nombre: 'Pantalón', precio: 40 },
+            { id: 3, nombre: 'Zapatos', precio: 60 },
+            { id: 4, nombre: 'Bolso', precio: 30 }
+        ]
 
-//ejemplos con if y else-----
+        // Función para mostrar todos los productos
+        const mostrarProductos = () => {
+            console.log('Productos disponibles:');
+            productos.forEach(producto => {
+                console.log(`ID: ${producto.id}, Nombre: ${producto.nombre}, Precio: $${producto.precio}`);
+            });
+        };
 
-const nombre = prompt("Ingresa tu nombre:");
+        // Función para buscar un producto por ID
+        const buscarProductoPorId = id => productos.find(producto => producto.id === id);
 
-if (nombre) {
-    console.log("¡Hola, " + nombre + "!");
-} else {
-    console.log("No ingresaste un nombre válido.");
-}
+        // Función para agregar un producto al carrito
+        const agregarAlCarrito = id => {
+            const producto = buscarProductoPorId(id);
+            if (producto) {
+                carrito.push(producto);
+                console.log(`Producto "${producto.nombre}" agregado al carrito.`);
+            } else {
+                console.log('Producto no encontrado.');
+            }
+
+            tomarAsistencia(alumno);
+        }
+
+    };
+
+    // Función para calcular el total del carrito
+    const calcularTotalCarrito = () => {
+        let total = 0;
+        carrito.forEach(producto => {
+            total += producto.precio;
+        });
+        return total;
+    };
+
+    // Array para almacenar los productos del carrito
+    const carrito = [];
+
+    // Iniciar el e-commerce
+    const iniciarEcommerce = () => {
+        mostrarProductos();
+
+        let continuar = true;
+        while (continuar) {
+            const opcion = parseInt(prompt('Ingrese el ID del producto que desea agregar al carrito (0 para salir):'));
+            if (opcion === 0) {
+                continuar = false;
+            } else {
+                agregarAlCarrito(opcion);
+            }
+        }
+
+        const totalCarrito = calcularTotalCarrito();
+        console.log(`Total del carrito: $${totalCarrito}`);
+    };
+
+    // Iniciar el e-commerce
+    iniciarEcommerce();
